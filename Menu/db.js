5315c14356ed0e81960d7d71c3307017e72a8874
Krjds9460 <87760368+Krjds9460@users.noreply.github.com>
2023-07-13 19:51:27 +0200

    Add files via upload

---
+++ Menu/db.js
@@ -0,0 +1,25 @@
+var data = {
+			  "italiano":
+			  {
+			    "MENU TURISTICI": "MENU TURISTICI",
+			    "ANTIPASTI": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non adipisci eligendi repellat ad dolor veritatis itaque sequi minus iste, doloremque. Officiis non eaque atque excepturi repudiandae nulla eos eligendi magni molestiae eius distinctio, voluptas pariatur incidunt et culpa inventore aspernatur recusandae nihil asperiores, vitae, maiores laborum quasi perspiciatis natus dignissimos! Accusantium aliquam nostrum impedit dignissimos iste, iure inventore! Sapiente, labore earum ut dicta ducimus asperiores laudantium natus officiis, quisquam placeat aspernatur voluptatum aut voluptates tenetur quos magni fugit quia. Fugiat."
+			  },
+			  "english":
+			  {
+					"men": "TOURISTIC MENU",
+			    "ant": "HORS D'OEUVRE",
+					"pri": "FIRST COURSE",
+					"sec": "SECOND COURSE",
+					"con": "GARNISH",
+					"alt": "MORE INFO"
+			  },
+			  "deutsch":
+			  {
+					"men": "TURISTISCHE MENU",
+					"ant": "KALTE VORSPEISEN",
+					"pri": "VORSPEISEN",
+					"sec": "HAUPTGERICHTE",
+					"con": "GARNISH",
+					"alt": "MEHR INFO"
+		  	}
+			}
